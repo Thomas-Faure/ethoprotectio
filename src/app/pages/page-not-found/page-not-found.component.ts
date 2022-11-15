a4a56d2c023e81ad2e07951633a9014971d08632
Thomas Faure <thomas.faure.bz@gmail.com>
2022-11-15 20:26:31 +0100

    amélioration

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { Router,NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  title = 'Clara';
+	public isMenuCollapsed = true;
+  currentRoute!: string;
 
+  
+
+  constructor(private router: Router) {
+    console.log(router.url);
+    this.router.events.pipe(
+      filter((event:any) => event instanceof NavigationEnd)
+    ).subscribe(x => this.currentRoute=x.url)
+
+  }
+
+  public getCurrentRoute(str : string){
+    return str == this.currentRoute;
+  }
   ngOnInit(): void {
+    
   }
 
 }
